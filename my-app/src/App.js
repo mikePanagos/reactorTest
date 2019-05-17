34a6cadddd39e6f80074c6580897f942a4e8b591
Michael Panagos <michael@technicool.com>
2019-05-17 00:39:55 -0500

    testing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>hello world</h1>
+      <h1>hello again</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
